39fbcd98a962bf5e91dcb42ad6dfcef6c990d0ec
Aleksandr <alex@Noutbuk-Alex.local>
2024-12-09 16:53:27 +0200

    Написал фн. для извлечения цифр из строки

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,5 @@
+const checkStringLength = (str, length) => str.length <= length;
+
 const isPalindrome = (str) => {
   let tmp = '';
 
@@ -11,4 +13,21 @@ const isPalindrome = (str) => {
   return reverseString === string;
 };
 
+const strChecker = (str) => {
+  const NUMBERS = '0123456789';
+  let result = '';
+
+  for (let i = 0; i <= str.length - 1; i++) {
+    for (let j = 0; j <= NUMBERS.length - 1; j++) {
+      if (NUMBERS[j] === str[i]) {
+        result += str[i];
+      }
+    }
+  }
+
+  return Math.floor(+result);
+};
+
+checkStringLength('проверяемая строка', 10);
 isPalindrome('Лёша на полке клопа нашёл');
+strChecker('а я томат');
